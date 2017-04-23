726c29687d577820db11075105817e6d3d307885
Caleb J. Yu <calebyu33@gmail.com>
2017-04-23 09:44:56 -0400

    added

---
+++ Graph.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Graph {
+
+    public Vertex[] vertices;
+    private GameObject[] nodes;
+
+    public Graph()
+    {
+        getNodes();
+        getNeighbors();
+        //printGraph();
+    }
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public void getNodes()
+    {
+        this.nodes = GameObject.FindGameObjectsWithTag("node");
+        this.vertices = new Vertex[nodes.Length];
+        for (int i = 0; i < nodes.Length; i++)
+        {
+            vertices[i] = new Vertex(i,nodes[i]);
+            /*vertices[i].vertex = nodes[i];
+            vertices[i].index = i;*/
+            //Debug.Log(vertices[i]);
+        }
+    }
+
+    public void getNeighbors()
+    {
+
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            RaycastHit hit1,hit2,hit3,hit4;
+            if (Physics.Raycast(vertices[i].vertex.transform.position, Vector3.forward, out hit1, 1.5f))
+            {
+                if (hit1.transform.gameObject.tag.Equals("node"))
+                {
+                    vertices[i].Neighbors.Add(new Neighbor(getVertexIndex(hit1.transform.gameObject),hit1.transform.gameObject));
+                }
+            }
+            if (Physics.Raycast(vertices[i].vertex.transform.position, Vector3.back, out hit2, 1.5f))
+            {
+                if (hit2.transform.gameObject.tag.Equals("node"))
+                {
+                    vertices[i].Neighbors.Add(new Neighbor(getVertexIndex(hit2.transform.gameObject),hit2.transform.gameObject));
+                }
+            }
+            if (Physics.Raycast(vertices[i].vertex.transform.position, Vector3.left, out hit3, 1.5f))
+            {
+                if (hit3.transform.gameObject.tag.Equals("node"))
+                {
+                    vertices[i].Neighbors.Add(new Neighbor(getVertexIndex(hit3.transform.gameObject),hit3.transform.gameObject));
+                }
+            }
+            Debug.DrawRay(vertices[i].vertex.transform.position,Vector3.right, Color.red, 10);
+            if (Physics.Raycast(vertices[i].vertex.transform.position, Vector3.right, out hit4, 1.5f))
+            {
+                if (hit4.transform.gameObject.tag.Equals("node"))
+                {
+                    vertices[i].Neighbors.Add(new Neighbor(getVertexIndex(hit4.transform.gameObject),hit4.transform.gameObject));
+                }
+            }
+        }
+    }
+
+    public void printGraph() {
+        foreach(Vertex v in vertices) {
+            Debug.Log(v.vertex.name + " : Neighbors: ");
+            for (int i = 0; i < v.Neighbors.Count; i++) {
+                Debug.Log(v.Neighbors[i] + "Position: " + v.Neighbors[i].vertex.transform.position);
+            }
+        }
+    }
+    public int getVertexIndex(GameObject gameObject) {
+        foreach(Vertex v in vertices) {
+            if(v.vertex == gameObject) {
+                return v.index;
+            }
+        }
+        return -1;
+    }
+    public class Vertex
+    {
+        public int index;
+        public GameObject vertex;
+        public List<Neighbor> Neighbors = new List<Neighbor>();
+        //public Neighbor neighbors; // adjacency linked lists for all vertices
+
+        public Vertex(int index,GameObject vertex)
+        {
+            this.index = index;
+            this.vertex = vertex;
+        }
+    }
+    public class Neighbor {
+
+        public int index;
+        public GameObject vertex;
+        public Neighbor(int index, GameObject vertex) {
+            this.index = index;
+            this.vertex = vertex;
+        }
+    }
+}
---
+++ MazeSolver.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MazeSolver : MonoBehaviour {
+
+    Stack<Vector3> points = new Stack<Vector3>();
+    Graph graph;
+    bool[] visited;
+    GameObject AI;
+    public Canvas canvas;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void solve()
+    {
+
+        Graph.Vertex start = null;
+        graph = new Graph();
+        for (int i=0;i<graph.vertices.Length;i++) //finds start node
+        {
+            //Debug.Log(graph.vertices[i]);
+            if (graph.vertices[i].vertex.name.Equals("Start"))
+            {
+                start = graph.vertices[i];
+            }
+            if (start != null)
+                break;
+        }
+        visited = new bool[graph.vertices.Length];
+        if (dfs(start, visited, graph.vertices))
+        {
+            Debug.Log("Path found");
+            canvas.GetComponent<Score>().showAITimeTrue();
+            if(AI!=null) {
+                Destroy(AI);
+            }
+            if (SceneManager.GetActiveScene().name.Equals("Competitive"))
+            {
+                StartCoroutine(wait());
+                GameObject Player = Instantiate(Resources.Load("Player"), start.vertex.transform.position, Quaternion.identity) as GameObject;
+            }
+            AI = Instantiate(Resources.Load("AI"),start.vertex.transform.position,Quaternion.identity) as GameObject;
+            AI.GetComponent<AIMovement>().receivePoints(points);
+        } else {
+            Debug.Log("No path");
+            canvas.GetComponentInChildren<Fade>().appearFade();
+        }
+    }
+    IEnumerator wait()
+    {
+        yield return new WaitForSeconds(3);
+    }
+    public bool dfs(Graph.Vertex start, bool[] visited, Graph.Vertex[] vertices)
+    {
+        if (vertices[start.index].Equals("Finish")) {
+            return true;
+        }
+        visited[start.index] = true;//INDEX HAS TO BE LOCATION IN VERTICES ARRAY
+        for (int i=0;i<vertices[start.index].Neighbors.Count;i++)//goes through neighbors List
+        {
+            //Debug.Log("test index: " + i);
+            //Debug.Log("visited[i]: " + visited[i]);    
+            if (!visited[vertices[start.index].Neighbors[i].index])
+            {
+                
+                if (vertices[start.index].Neighbors[i].vertex.name.Equals("Finish"))
+                {
+                    points.Push(vertices[start.index].Neighbors[i].vertex.transform.position);
+                    return true;
+                }
+                if(dfs(vertices[vertices[start.index].Neighbors[i].index], visited, vertices)) {
+                    points.Push(vertices[start.index].Neighbors[i].vertex.transform.position);
+                    return true;
+                }
+
+            }
+        }
+        return false;
+    }
+    
+}
